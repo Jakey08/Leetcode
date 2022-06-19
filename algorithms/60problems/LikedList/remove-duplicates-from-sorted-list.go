e8e3984a460fb0c54412e08966722c7032a7a8ed
Jakey08 <yuweb1105@gmail.com>
2022-06-20 01:26:25 +0900

    add struct

--- algorithms/60problems/LikedList/remove-duplicates-from-sorted-list.go
+++ algorithms/60problems/LikedList/remove-duplicates-from-sorted-list.go
@@ -1,15 +1,21 @@
-
-func deleteDuplicates(head *ListNode) *ListNode {
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+ 
+ func deleteDuplicates(head *ListNode) *ListNode {
 	prev := &ListNode{}
 	prev.Next = head
 	cur := head
 	for cur != nil {
 		next := cur.Next
-		for next != nil && next.Val == cul.Val {
+		for next != nil && next.Val == cur.Val {
 			next = next.Next
 		}
 		cur.Next = next
 		cur = next
 	}
 	return prev.Next
-}
